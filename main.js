6ab3293ce7898eded42f3e5ab95b0a2d0d0e40fe
EmilioSanchezLernia <emilio.sanchez@lerniastudent.se>
2023-11-13 11:03:59 +0100

    Stopped scrolling when menu is active

--- main.js
+++ main.js
@@ -4,11 +4,14 @@ const menu = document.querySelector('.menu');
 document.querySelector('.hamburgerMenu').addEventListener('click', function () {
     if (menu.classList.contains('hidden')) {
         menu.classList.remove('hidden')
+        document.querySelector('body').classList.add('noScroll')
     } else {
         menu.classList.add('hidden')
+        document.querySelector('body').classList.remove('noScroll')
     }
 })
 
 document.querySelector('.closeOverlay').addEventListener('click', function () {
     menu.classList.add('hidden')
+    document.querySelector('body').classList.remove('noScroll')
 })
